a04a3dd2f1f4492422e9143b3891b6c70545d3bb
GuangyaoDou <58749007+GuangyaoDou@users.noreply.github.com>
2020-03-30 09:33:45 -0700

    Initialze Programs

---
+++ src/Main/AllStayAtHome.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class AllStayAtHome extends Population{
+
+  public AllStayAtHome(int n){
+    super(n);
+  }
+
+  public void createPeople(){
+    for(int i=0; i<this.people.length; i++){
+      this.addPerson(new StayAtHome());
+    }
+  }
+}
---
+++ src/Main/StayAtHome.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class StayAtHome extends Person{
+
+
+	public StayAtHome() {
+		super();
+	}
+
+	public void tryToMove() {
+		// don't move
+	}
+
+}
